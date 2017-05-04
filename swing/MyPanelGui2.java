71fc3eb74206d339097298759dd18dec19448b3d
xiaofengzheng <ilovepeggy88@gmail.com>
2017-05-04 18:03:09 +0800

    MyPanelGui2, BorderLayout.SOUTH,WEST,EAST,NORTH,CENTER

--- swing/MyPanelGui2.java
+++ swing/MyPanelGui2.java
@@ -4,25 +4,35 @@ import java.awt.event.*;
 
 public class MyPanelGui2 {
     JFrame frame = null;
-    JButton button = null;
+    JLabel label = null;
 public static void main(String[] args) {
-    MyPanelGui gui = new MyPanelGui();
+    MyPanelGui2 gui = new MyPanelGui2();
     gui.go();
 }
 public void go() {
     frame = new JFrame();
-    MyOvalGradientPanel panel = new MyOvalGradientPanel();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    button = new JButton("click me");
-    button.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent event) {
-            button.setText("I've been clicked!");
-            frame.repaint();
-        }
-        });
-    frame.getContentPane().add(BorderLayout.EAST, button);
+    MyOvalGradientPanel panel = new MyOvalGradientPanel();
+    JButton buttonLabel = new JButton("change label");
+    JButton buttonColor = new JButton("change circle");
+    label = new JLabel("a label~");
+    buttonLabel.addActionListener(new LabelListener());
+    buttonColor.addActionListener(new ColorListener());
+    frame.getContentPane().add(BorderLayout.WEST, label);
     frame.getContentPane().add(BorderLayout.CENTER, panel);
+    frame.getContentPane().add(BorderLayout.EAST, buttonLabel);
+    frame.getContentPane().add(BorderLayout.SOUTH, buttonColor);
     frame.setSize(300, 300);
     frame.setVisible(true);
+}
+class LabelListener implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+        label.setText("has been changed");
+    }
+}
+class ColorListener implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+        frame.repaint();
+    }
 }
 }
